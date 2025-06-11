28c7244a16afc6300f6bf646da915044e7b5f370
Jordan Bravo <jordan@bravo.cc>
2025-06-11 17:44:39 +0000

    saving progress

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,8 +1,8 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
-    // let a = ???
+    let my_array = ["Element"; 100];
 
-    if a.len() >= 100 {
+    if my_array.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
